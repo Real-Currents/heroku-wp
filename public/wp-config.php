874ee11257e4a4aac954b52f88a87270dbe6126b
John <john@real-currents.com>
2018-05-29 08:41:09 -0400

    hide warning messages

--- public/wp-config.php
+++ public/wp-config.php
@@ -14,10 +14,13 @@
  * @package WordPress
  */
 
-error_reporting(E_ERROR);
-
 // Setup autoload
 require '/app/vendor/autoload.php';
+
+ini_set('display_errors','Off');
+ini_set('error_reporting', E_ALL );
+define('WP_DEBUG', false);
+define('WP_DEBUG_DISPLAY', false);
 
 // Disable filesystem level changes from WP
 define( 'DISALLOW_FILE_EDIT', true );
